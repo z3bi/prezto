927a8a9eca37bbf4ce1be817ee810323990a937a
Ameir Al-Zoubi <ameir.alzoubi@gmail.com>
2019-08-03 12:31:08 -0400

    color change

--- modules/utility/init.zsh
+++ modules/utility/init.zsh
@@ -99,12 +99,12 @@ else
   if zstyle -t ':prezto:module:utility:ls' color; then
     # Define colors for BSD ls if they're not already defined
     if [[ -z "$LSCOLORS" ]]; then
-      export LSCOLORS='gxfxcxdxbxGxDxabafacad'
+      export LSCOLORS='exfxcxdxbxGxDxabagacad'
     fi
 
     # Define colors for the completion system if they're not already defined
     if [[ -z "$LS_COLORS" ]]; then
-      export LS_COLORS='di=36:ln=35:so=32:pi=33:ex=31:bd=34;01:cd=33;01:su=31;40;07:sg=34;40;07:tw=32;40;07:ow=33;40;07:'
+      export LS_COLORS='di=34:ln=35:so=32:pi=33:ex=31:bd=36;01:cd=33;01:su=31;40;07:sg=36;40;07:tw=32;40;07:ow=33;40;07:'
     fi
 
     alias ls="${aliases[ls]:-ls} -G"
